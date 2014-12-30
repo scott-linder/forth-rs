d58d2418158e2e83f53c7350d4e4bfa6f2d15ad8
scott-linder <scott.b.linder@wmich.edu>
2014-12-30 10:34:19 -0500

    Remove need for Rc pointer in State

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,6 @@
 use ForthResult;
 use error::Error::UnknownWord;
 use stack::Stack;
-use std::rc::Rc;
 use std::result::Result::{Ok, Err};
 use word::Word;
 use word::WordKind::{Builtin, Words};
@@ -9,7 +8,7 @@ use word::WordKind::{Builtin, Words};
 #[deriving(Default)]
 pub struct State {
     stack: Stack,
-    dict: Vec<Rc<Word>>,
+    dict: Vec<Word>,
 }
 
 impl State {
@@ -21,34 +20,29 @@ impl State {
     }
 
     pub fn add_word(&mut self, word: Word) {
-        self.dict.push(Rc::new(word));
+        self.dict.push(word);
+    }
+
+    fn real_run_word(dict: &Vec<Word>, stack: &mut Stack,
+                     command: &str) -> ForthResult {
+        for word in dict.iter() {
+            if command == word.command {
+                match word.kind {
+                    Builtin(ref f) => try!((*f)(stack)),
+                    Words(ref ws) => for w in ws.iter() {
+                        try!(State::real_run_word(dict, stack, w.as_slice()));
+                    },
+                }
+                return Ok(());
+            }
+        }
+        let i = try!(command.parse().ok_or(UnknownWord));
+        stack.push(i);
+        Ok(())
     }
 
     pub fn run_word(&mut self, command: &str) -> ForthResult {
-        let mut next = None;
-        for word in self.dict.iter() {
-            if command == word.command {
-                next = Some(word.clone());
-                break;
-            }
-        }
-        match next {
-            Some(word) => {
-                match word.kind {
-                    Builtin(ref f) => try!((*f)(&mut self.stack)),
-                    Words(ref ws) => for w in ws.iter() {
-                        try!(self.run_word(w.as_slice()))
-                    },
-                }
-            },
-            None => {
-                match command.parse() {
-                    Some(i) => self.stack.push(i),
-                    None => return Err(UnknownWord),
-                }
-            },
-        }
-        Ok(())
+        State::real_run_word(&self.dict, &mut self.stack, command)
     }
 
     pub fn parse_line(&mut self, line: &str) -> ForthResult {
